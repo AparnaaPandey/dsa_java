213ad9a82e3d426852be9238b7ad8735ee13cc72
AparnaaPandey <aparna6119@gmail.com>
2025-07-02 18:42:36 +0530

    pattern questions -update2

---
+++ patterns_ques/ques7.java
@@ -0,0 +1,15 @@
+public class ques7 {
+    public static void main(String[] args) {
+        
+        int n =5;
+       
+        for(int i =1; i<=n;i++){
+             int m=1;
+            for(int j=1;j <=n-i+1;j++){
+                System.out.print(m+" ");
+                m++;
+                      }
+                    System.out.println();          }
+        }
+    
+}
